ac0b61c3497c1b038a9d25a70bba6639d5945415
𝐀ʟᴘʜᴀ 𝐁ᴀʙʏ <168528642+TEAMPURVI@users.noreply.github.com>
2024-12-07 17:53:18 +0530

    ᴘᴜʀᴠɪ

--- PURVIMUSIC/utils/thumbnails.py
+++ PURVIMUSIC/utils/thumbnails.py
@@ -11,7 +11,7 @@ from PIL import ImageFilter, ImageFont, ImageOps
 from unidecode import unidecode
 from youtubesearchpython.__future__ import VideosSearch
 
-from SONALI import app
+from PURVIMUSIC import app
 from config import YOUTUBE_IMG_URL
 
 
