08b3311fa7319bff4ce07eb435c5cfa23602067b
Dvis <gokhaledurgesh4@gmail.com>
2025-01-14 11:04:07 +0530

    Update stream.py

--- PURVIMUSIC/utils/stream/stream.py
+++ PURVIMUSIC/utils/stream/stream.py
@@ -98,7 +98,7 @@ async def stream(
                     "video" if video else "audio",
                     forceplay=forceplay,
                 )
-                 try:
+                 except:
                     img = await get_thumb(vidid)
                     button = stream_markup(_, chat_id)
                     run = await app.send_photo(
