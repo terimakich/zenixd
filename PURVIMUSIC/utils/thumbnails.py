5de30379b7b0987f8e81790d21f5dfb7cefa4f97
RAUSHAN-SONALI-143 <156349324+RAUSHAN-SONALI-143@users.noreply.github.com>
2025-01-31 02:42:54 +0530

    Update thumbnails.py

--- PURVIMUSIC/utils/thumbnails.py
+++ PURVIMUSIC/utils/thumbnails.py
@@ -73,8 +73,8 @@ async def get_thumb(videoid):
         draw = ImageDraw.Draw(background)
         arial = ImageFont.truetype("PURVIMUSIC/assets/font2.ttf", 30)
         font = ImageFont.truetype("PURVIMUSIC/assets/font.ttf", 30)
-        alpha.name = TEAM PURVI BOTS
-        draw.text((1110, 8), unidecode(alpha.name), fill="yellow", font=arial)
+        text_size = draw.textsize("TEAM PURVI BOTS   ", font=font)
+        draw.text((1280 - text_size[0] - 10, 10), "TEAM PURVI BOTS   ", fill="yellow", font=font)
         draw.text(
             (55, 560),
             f"{channel} | {views[:23]}",
