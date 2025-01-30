9c9c561573e02a4e3cd06df031cd231b11fdbe85
RAUSHAN-SONALI-143 <156349324+RAUSHAN-SONALI-143@users.noreply.github.com>
2025-01-30 23:47:57 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/TEAMPURVI/SONALI",
+    "https://github.com/RAUSHAN-SONALI-143/SONALI",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
