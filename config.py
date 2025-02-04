e9c03dfe7822c389dade66a07d59196328333df9
SONA-RAJA <botspurvi@gmail.com>
2025-02-04 13:54:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -53,7 +53,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 # ----------------------------------------------------------------
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/RAUSHAN-SONALI-143/SONALI",
+    "https://github.com/SONA-RAJA/SONALI",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 GIT_TOKEN = getenv(
