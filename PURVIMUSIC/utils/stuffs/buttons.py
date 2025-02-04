cb999e77e45fc2ed9aef0f5e5a36b1452682d66c
SONA-RAJA <botspurvi@gmail.com>
2025-02-04 20:57:41 +0530

    Update buttons.py

--- PURVIMUSIC/utils/stuffs/buttons.py
+++ PURVIMUSIC/utils/stuffs/buttons.py
@@ -4,18 +4,18 @@ from pyrogram import Client, filters, enums
 class BUTTONS(object):
     ABUTTON = [
     [
-        InlineKeyboardButton("˹ sᴜᴘᴘᴏꝛᴛ ˼", url="https://t.me/PURVI_SUPPORT"),
-        InlineKeyboardButton("˹ ᴜᴘᴅᴧᴛᴇ ˼", url="https://t.me/+gMy8Cp190ediNzZl")
+        InlineKeyboardButton("⌯ sᴜᴘᴘᴏꝛᴛ ⌯", url="https://t.me/PURVI_SUPPORT"),
+        InlineKeyboardButton("⌯ ᴜᴘᴅᴧᴛᴇ ⌯", url="https://t.me/+gMy8Cp190ediNzZl")
     ],
     [
-        InlineKeyboardButton("˹ sᴏᴜʀᴄᴇ ˼", callback_data="gib_source"),
-        InlineKeyboardButton("˹ ʙᴀᴄᴋ ˼", callback_data=f"settingsback_helper")
+        InlineKeyboardButton("⌯ sᴏᴜʀᴄᴇ ⌯", callback_data="gib_source"),
+        InlineKeyboardButton("⌯ ʙᴀᴄᴋ ⌯", callback_data=f"settingsback_helper")
     ]
     ]
 
-    UBUTTON = [[InlineKeyboardButton("˹ ᴍᴜsɪᴄ ˼", callback_data="settings_back_helper"),InlineKeyboardButton("˹ ᴛᴏᴏʟs ˼", callback_data=f"tbot_cb")],[InlineKeyboardButton("˹ ʙᴧᴄᴋ ˼", callback_data=f"settingsback_helper"),
+    UBUTTON = [[InlineKeyboardButton("⌯ ᴍᴜsɪᴄ ⌯", callback_data="settings_back_helper"),InlineKeyboardButton("⌯ ᴛᴏᴏʟs ⌯", callback_data=f"tbot_cb")],[InlineKeyboardButton("⌯ ʙᴧᴄᴋ ⌯", callback_data=f"settingsback_helper"),
     ]]
 
-    TBUTTON = [[InlineKeyboardButton("˹ ᴀᴄᴛɪᴠᴇ ˼", callback_data="cplus HELP_active"),InlineKeyboardButton("˹ ᴀᴜᴛʜ ˼", callback_data="cplus HELP_auth"),InlineKeyboardButton("˹ ʙʟᴏᴄᴋ ˼", callback_data="cplus HELP_block")],[InlineKeyboardButton("˹ chat ˼", callback_data="cplus HELP_chat"),
-    InlineKeyboardButton("˹ ɢ-ᴄᴀsᴛ ˼", callback_data="cplus HELP_gcast")],[InlineKeyboardButton("˹ ʙᴧᴄᴋ ˼", callback_data=f"ubot_cb"),
+    TBUTTON = [[InlineKeyboardButton("⌯ ᴀᴄᴛɪᴠᴇ ⌯", callback_data="cplus HELP_active"),InlineKeyboardButton("⌯ ᴀᴜᴛʜ ⌯", callback_data="cplus HELP_auth"),InlineKeyboardButton("⌯ ʙʟᴏᴄᴋ ⌯", callback_data="cplus HELP_block")],[InlineKeyboardButton("⌯ ᴄʜᴀᴛ ʙᴏᴛ ⌯", callback_data="cplus HELP_chat"),
+    InlineKeyboardButton("⌯ ɢ-ᴄᴀsᴛ ⌯", callback_data="cplus HELP_gcast")],[InlineKeyboardButton("⌯ ʙᴧᴄᴋ ⌯", callback_data=f"ubot_cb"),
     ]]
