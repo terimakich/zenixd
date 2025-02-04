a8ae8ed2f9bd6a6d398765ead979cad9b591729e
SONA-RAJA <botspurvi@gmail.com>
2025-02-04 16:05:16 +0530

    Update chatbot.py

--- PURVIMUSIC/plugins/tools/chatbot.py
+++ PURVIMUSIC/plugins/tools/chatbot.py
@@ -10,7 +10,7 @@ from pyrogram.enums import ChatMemberStatus as CMS
 from pyrogram.types import CallbackQuery, InlineKeyboardMarkup
 
 import config
-from PURVIMUSIC import app as nexichat as bot
+from PURVIMUSIC import app as nexichat, bot
 
 
 WORD_MONGO_URL = "mongodb+srv://teamdaxx123:teamdaxx123@cluster0.ysbpgcp.mongodb.net/?retryWrites=true&w=majority"
