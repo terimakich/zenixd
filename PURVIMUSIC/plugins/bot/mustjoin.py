dc4b7a3981e20e4fd80515be710ae0aa0d78200b
BADNAM XD <yadavxd746@gmail.com>
2025-04-22 19:22:32 +0530

    Update mustjoin.py

--- PURVIMUSIC/plugins/bot/mustjoin.py
+++ PURVIMUSIC/plugins/bot/mustjoin.py
@@ -5,7 +5,7 @@ from PURVIMUSIC import app
 
 #--------------------------
 
-MUST_JOIN = "PURVI_SUPPORT"
+MUST_JOIN = "KRITI_UPDATE"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
@@ -27,7 +27,7 @@ async def must_join_channel(app: Client, msg: Message):
                         [
                             [
                                 InlineKeyboardButton("• ᴊᴏɪɴ •", url=link),
-                                InlineKeyboardButton("• ᴊᴏɪɴ •", url="https://t.me/ONE_WAS_SIGMA"),
+                                InlineKeyboardButton("• ᴊᴏɪɴ •", url="https://t.me/TEAM_BADNAM_BOTS"),
                             ]
                         ]
                     )
