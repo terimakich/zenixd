50d7a57280984b2b3aeccf854a2c814c94f220ad
SAMHEHE1 <sampawaryt@gmail.com>
2025-08-07 16:11:44 +0530

    Update mustjoin.py

--- PURVIMUSIC/plugins/bot/mustjoin.py
+++ PURVIMUSIC/plugins/bot/mustjoin.py
@@ -5,7 +5,7 @@ from PURVIMUSIC import app
 
 #--------------------------
 
-MUST_JOIN = "KRITI_UPDATE"
+MUST_JOIN = "TEAMKAISEN"
 #------------------------
 @app.on_message(filters.incoming & filters.private, group=-1)
 async def must_join_channel(app: Client, msg: Message):
